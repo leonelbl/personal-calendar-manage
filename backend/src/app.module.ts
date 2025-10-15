a996e37ba060e046cc531c49ef5ccb133212c9e7
Brayan Hernandez <leonelhernandezbl@gmail.com>
2025-10-14 22:56:40 -0600

    feat(backend): bookings - implement CRUD functionality with validations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/database/database.module';
 import { AuthModule } from './presentation/controllers/auth/auth.module';
+import { BookingsModule } from './presentation/controllers/bookings/bookings.module';
 
 @Module({
   imports: [
     DatabaseModule,
-    AuthModule
+    AuthModule,
+    BookingsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
